60c84371761969ad353e0bd485602e228784d426
N3t4Ds <netad_work@sina.com>
2025-03-17 15:52:07 +0800

    add poweroff reboot

--- poweroff/pwsd_poweroff_listener.py
+++ poweroff/pwsd_poweroff_listener.py
@@ -1,4 +1,5 @@
 import os
+
 import serial
 
 ser = serial.Serial('/dev/ttyS0', 9600)  # 根据实际串口设备修改
@@ -7,3 +8,5 @@ while True:
     data = ser.readline().decode().strip()
     if data == "poweroff":
         os.system("sudo shutdown -h now")  # 执行关机命令
+    elif data == "reboot":
+        os.system("sudo reboot")  # 执行重启命令
